5f1b6d2ed5c5534d287e0e212f41e7fe74b651eb
Michael Maier <maier1michael@gmail.com>
2017-04-22 19:38:12 +0200

    RxCpp 4.0.0

---
+++ conanfile.py
@@ -0,0 +1,20 @@
+from conans import ConanFile
+
+class RxCppConan(ConanFile):
+  name = "RxCpp"
+  version = "4.0.0"
+  settings = None
+  build_policy = "missing"
+  url = "https://github.com/Reactive-Extensions/RxCpp"
+  description = "The Reactive Extensions for C++ (RxCpp) is a library of algorithms for values-distributed-in-time."
+  license = "Apache License 2.0"
+
+  def source(self):
+    self.run("git clone https://github.com/Reactive-Extensions/RxCpp")
+    self.run("cd RxCpp && git checkout v4.0.0")
+  
+  def build(self):
+    pass
+
+  def package(self):
+    self.copy("*.hpp", dst="include", src="RxCpp/Rx/v2/src", keep_path=True)
